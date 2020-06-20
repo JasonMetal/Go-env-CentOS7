717fa72af8ac98954b10d3aed26cea5753f8f534
JasonMetal <935216773@qq.com>
2020-06-20 09:59:04 +0800

    update-centos7-go

--- InstallGoland.sh
+++ InstallGoland.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#Install Go Lang in CentOS 7 machines.
+##Install Go Lang in CentOS 7 machines.
 #Installing imp packages
 yum install wget unzip -y
 ##Donwloading Go Package
 wget -O /opt/go1.14.4.linux-amd64.tar.gz https://storage.googleapis.com/golang/go1.14.4.linux-amd64.tar.gz
-#Extract packages to a location
+##Extract packages to a location
 tar -C /usr/local -xzf /opt/go1.14.4.linux-amd64.tar.gz
 ##Export Variable path
 echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
